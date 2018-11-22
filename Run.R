a2ad24499197d8ecab8af866a2895f0a93094ad9
aaronweinstock <weinstoa@oregonstate.edu>
2018-11-22 04:28:05 +0000

    Running the simulation

---
+++ Run.R
@@ -0,0 +1,24 @@
+# install.packages("devtools") #to load simulation functions
+# install.packages("roxygen2") #to access NFL_Playoff_Probabilities documentation
+library(roxygen2)
+devtools::load_all()
+
+# If you desire to use any functions in NFLSim individually (outside of
+# NFL_Playoff_Probabilities), call the function "dependencies()" before
+# use; this will load/request install for all required packages for 
+# using NFLSim [this function is called automatically by 
+# NFL_Playoff_Probabilities]
+
+# Run this line to see documentation for the simulation function
+?NFL_Playoff_Probabilities
+
+# Execute the simulation
+playoff_probs = NFL_Playoff_Probabilities(initialization_start = 2017,
+                                          season_to_sim = 2018,
+                                          week_from = 12,
+                                          cval = 40.29,
+                                          nsim = 50,
+                                          updates = TRUE,
+                                          plot = TRUE)
+# Observe results in table format
+playoff_probs
